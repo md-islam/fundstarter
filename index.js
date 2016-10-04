58ab50656df45fcc8b00ae007705d37c9024b7fc
Ubuntu <ubuntu@ip-172-31-7-184.us-west-2.compute.internal>
2016-10-04 05:44:48 +0000

    modularized and added routing, 1-a, 1-b, and 2

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 	var http = require("http");
 	var fs = require("fs");
-	//web server object
+	var filePath = 'public/index.html';
+	var Router = require('router');
+	var finalhandler = require('finalhandler')
+	var router = Router()
+
+
+	// blocking code example
+	router.get('/1-a', function(req, res){
+		var htmlSync = fs.readFileSync('public/index.html');
+		res.setHeader('Content-Type', 'text/html, charset=utf-8');
+		res.end(htmlSync);
+	});
+
+	// non blocking code example
+	router.get('/1-b', function(req, res){
+		fs.readFile('public/index.html', function(err, html) {
+			if (err) {
+				throw err;
+			} else {
+				console.log("app started");
+			}
+			res.setHeader('Content-Type', 'text/html, charset=utf-8');
+			res.end(html);
+		});
+	});
 
 
 
-	// Non blocking code example 
-	fs.readFile('public/index.html', function(err, html) {
-	    if (err) {
-	        throw err;
-	    } else {
-	        console.log("app started");
-	    }
-
-	    http.createServer(function(request, response) {
-	        response.writeHead(200, {
-	            'Content-Type': 'text/html'
-	        });
-	        response.write(html)
-	        response.end();
-	    }).listen(process.env.PORT||8080); //let's listen for both local and also make it compatible for Heroku
-
+	//this part checks if file exists and then does 
+	router.get('/2', function(req, res){
+		fs.exists(filePath, function(exists) {
+			if (exists) {
+				fs.stat(filePath, function(error, stats) {
+					fs.open(filePath, "r", function(error, fd) {
+						var buffer = new Buffer(stats.size);
+						fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+							var data = buffer.toString("utf8", 0, buffer.length);
+							res.end(data);
+							fs.close(fd);
+						});
+					});
+				});
+			}
+		});
 
 	});
 
@@ -30,6 +54,23 @@
 	// 	res.writeHead(200, {'Content-Type': 'text/html'});
 	//   	res.end(htmlSync);
 	// }).listen(8080);
+
+	
+
+
+	
+
+	var server = http.createServer(function(req, res) {
+		router(req, res, finalhandler(req, res));
+	})
+
+	server.listen(process.env.PORT||8080);
+
+	// Non blocking code example 
+	
+
+
+	
 
 
 
@@ -49,86 +90,85 @@
 
 
 
-	fs.access('public/index.html', fs.F_OK, function(err) {
-	    if (!err) {
-	        // Do something
+	// fs.access('public/index.html', fs.F_OK, function(err) {
+	// 	if (!err) {
+	//         // Do something
 
 
-	        //fs.stat section to get file info --[START]
-	        console.log("Going to get file info!");
-	        fs.stat('public/index.html', function(err, stats) {
-	            if (err) {
-	                return console.error(err);
-	            }
-	            console.log(stats);
-	            console.log("Got file info successfully!");
+	//         //fs.stat section to get file info --[START]
+	//         console.log("Going to get file info!");
+	//         fs.stat('public/index.html', function(err, stats) {
+	//         	if (err) {
+	//         		return console.error(err);
+	//         	}
+	//         	console.log(stats);
+	//         	console.log("Got file info successfully!");
 
-	            // Check file type
-	            console.log("isFile ? " + stats.isFile());
-	            console.log("isDirectory ? " + stats.isDirectory());
-	        });
+	//             // Check file type
+	//             console.log("isFile ? " + stats.isFile());
+	//             console.log("isDirectory ? " + stats.isDirectory());
+	//         });
 
-	        //fs.stat section to get file info --[END]
-	    } else {
-	        // It isn't accessible
-	        return console.error(err);
-	    }
-	});
-
-
-	fs.access('public/index.html', fs.F_OK, function(err) {
-	    if (!err) {
-	        // Do something
+	//         //fs.stat section to get file info --[END]
+	//     } else {
+	//         // It isn't accessible
+	//         return console.error(err);
+	//     }
+	// });
 
 
-	       
-	        //fs.open() followed by fs.read() section -- [START]
-	        var buf = new Buffer(1024);
+	// fs.access('public/index.html', fs.F_OK, function(err) {
+	// 	if (!err) {
+	//         // Do something
 
-	        console.log("Going to open an existing file");
-	        fs.open('public/index.html', 'r+', function(err, fd) {
-	            if (err) {
-	                return console.error(err);
-	            }
-	            console.log("File opened successfully!");
-	            console.log("Going to read the file");
 
-	            fs.read(fd, buf, 0, buf.length, 0, function(err, bytes) {
-	                if (err) {
-	                    console.log(err);
-	                }
 
-	                // Print only read bytes to avoid junk.
-	                if (bytes > 0) {
-	                    console.log(buf.slice(0, bytes).toString());
-	                }
+	//         //fs.open() followed by fs.read() section -- [START]
+	//         var buf = new Buffer(1024);
 
-	                // Close the opened file.
-	                fs.close(fd, function(err) {
-	                    if (err) {
-	                        console.log(err);
-	                    }
-	                    console.log("File closed successfully.");
-	                });
-	            });
-	        });
+	//         console.log("Going to open an existing file");
+	//         fs.open('public/index.html', 'r+', function(err, fd) {
+	//         	if (err) {
+	//         		return console.error(err);
+	//         	}
+	//         	console.log("File opened successfully!");
+	//         	console.log("Going to read the file");
 
-	        //fs.open() followed by fs.read() section -- [end]
-	    } else {
-	        // It isn't accessible
-	        return console.error(err);
-	    }
-	});
+	//         	fs.read(fd, buf, 0, buf.length, 0, function(err, bytes,buffer) {
+	//         		if (err) {
+	//         			console.log(err);
+	//         		}
+
+	//                 // Print only read bytes to avoid junk.
+	//                 if (bytes > 0) {
+	//                 	console.log(buf.slice(0, bytes).toString());
+	//                 	http.createServer(function(request, response) {
+	//                 		response.writeHead(200, {
+	//                 			'Content-Type': 'text/html'
+	//                 		});
+	//                 		response.write(buf.slice(0, bytes).toString('utf8', 0, buffer))
+	//                 		response.end();
+	//                 	}).listen(process.env.PORT||8080);
+	//                 }
 
 
 
 
+	//                 // Close the opened file.
+	//                 fs.close(fd, function(err) {
+	//                 	if (err) {
+	//                 		console.log(err);
+	//                 	}
+	//                 	console.log("File closed successfully.");
+	//                 });
+	//             });
+	//         });
 
-
-
-
-
-
-
+	//         //fs.open() followed by fs.read() section -- [end]
+	//     } else {
+	//         // It isn't accessible
+	//         return console.error(err);
+	//     }
+	// });
 
 	//fs.open and fs.read section
